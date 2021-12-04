0e1475123f13c6337e09a7c307e9f6b9f218892b
Rian Dias de Oliveira <riandiasdeoliveira2001@gmail.com>
2021-12-04 19:16:28 -0300

    Create menu button script

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+const showNavigationMenu = () => {
+    if (navigationMenu.style.display === "flex") {
+        navigationMenu.style.display = "none";
+    } else {
+        navigationMenu.style.display = "flex";
+    }
+}
