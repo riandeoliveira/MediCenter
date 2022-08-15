e7e5541136f2f8bf76eb4f2917c28bb157f82cd7
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-14 21:10:49 -0300

    feat: add Banner to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
+import Banner from "templates/Banner";
 import Header from "templates/Header";
 
 const Home: NextPage = (): JSX.Element => {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 };
