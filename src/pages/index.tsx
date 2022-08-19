0027a65b5c7c1fa869a514ac34d3a4916849c568
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-19 11:11:44 -0300

    fix(banner): fix mobile height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
+import Title from "components/Title";
 import type { NextPage } from "next";
 import Banner from "templates/Banner";
 import Header from "templates/Header";
+import Layout from "templates/Layout";
 
 const Home: NextPage = (): JSX.Element => {
   return (
     <>
       <Header />
       <Banner />
+      <Layout>
+        <div>a</div>
+        <div>b</div>
+      </Layout>
+      <Title name="Latest News" theme="dark" />
     </>
   );
 };
