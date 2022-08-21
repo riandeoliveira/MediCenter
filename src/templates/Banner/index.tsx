789dcedf8f2bc5d4ec786dae3d4cc129e65128ac
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-21 19:17:52 -0300

    style: fix LinkableCard content

--- src/templates/Banner/index.tsx
+++ src/templates/Banner/index.tsx
@@ -22,7 +22,7 @@ const Banner = (): JSX.Element => {
             call emergency appointment number for emergency service.
           </LinkableCard>
           <LinkableCard label="Doctors Timetable" theme="neutral">
-            Here at medicenter we have individual doctor`&#39;`s lists. Click
+            Here at medicenter we have individual doctor&#39;s lists. Click
             read more below to see services and current timetable for our
             doctors.
           </LinkableCard>
