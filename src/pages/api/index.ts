b014d5f52f6efc237dd8a09b52e54f35c2244283
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-22 14:27:05 -0300

    refactor: remove unnecessary api dir

--- src/pages/api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).send({ name: "Hello, Rian Oliveira! Have a nice coding" });
-};
-
-export default handler;
